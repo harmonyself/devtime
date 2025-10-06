ee7e28f6d619a73715bd92c84a8b801703cb2df5
harmonyself <harmonyself@gmail.com>
2025-10-07 01:40:35 +0900

    안내 문구 수정

--- app/page.tsx
+++ app/page.tsx
@@ -18,11 +18,11 @@ export default function Home() {
           <p className="text-gray-600 text-lg">
             개발 시간을 추적하고 수익을 계산하세요
           </p>
-          <p className="text-gray-600 text-lg">
+          <p className="text-gray-300 text-lg">
             현재는 LocalStorage 사용으로 동일 브라우저에서만 프로젝트가 유지됩니다.
           </p>          
-          <p className="text-gray-600 text-lg">
-            현재는 무료로 제공되며, 동기화 가능한 Pro 버전은 곧 예정입니다!
+          <p className="text-gray-300 text-lg">
+            현재는 무료로 제공되며, 동기화 가능한 Pro 버전은 곧 출시할 예정입니다!
           </p>          
           
         </header>
